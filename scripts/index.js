69f1ac49d338562b5eecf619d7add44994e22bc1
unknown <bev030997@gmail.com>
2020-03-26 11:29:49 +0300

    add styles and scroll animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const scrollPage = () => {
+    document.body.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target.closest('.header__navbar-link') || target.closest('.footer__button')) {
+            event.preventDefault();
+            const elem = target.getAttribute('href');
+
+            document.querySelector(elem).scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+};
+scrollPage();
+
 const progressBar = () => {
     const progress = document.querySelectorAll('.skills__progress-done');
 
